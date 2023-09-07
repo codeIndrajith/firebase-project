701da88ed0690a99252f446b29e5686442d53ec0
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2023-09-07 12:53:57 +0530

    Complete header component
    update the name of user in authentication.

--- realtor-clone/src/components/Header.jsx
+++ realtor-clone/src/components/Header.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import { useLocation , useNavigate } from 'react-router-dom'
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import React, { useEffect, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom';
+
 function Header() {
+    const [pageName , setPageName] = useState("Sign In");
     const location = useLocation();
-    console.log(location)
     const navigate = useNavigate();
-    const pathMathRoute = (route) => {
+    const auth = getAuth();
+    useEffect(()=> {
+        onAuthStateChanged(auth , (user) => {
+            if(user) {
+                setPageName("Profile");
+            }else {
+                setPageName("Sign In")
+            }
+        })
+    },[auth])
+    const pathMatchRoute = (route) => {
        if(route === location.pathname) {
         return true;
        }
@@ -18,9 +30,9 @@ function Header() {
             </div>
             <div>
                 <ul className='cursor-pointer flex justify-between space-x-9'>
-                    <li className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/") && "text-black border-b-red-500"}`} onClick={() => navigate("/")}>Home</li>
-                    <li className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/offers") && "text-black border-b-red-500"}`} onClick={() => navigate("/offers")}>Offers</li>
-                    <li className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/sign-in") && "text-black border-b-red-500"}`} onClick={() => navigate("/sign-in")}>Sign In</li>
+                    <li className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute('/') && "text-blue-900 border-b-red-800"}`} onClick={() => navigate("/")}>Home</li>
+                    <li className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute('/offers') && "text-blue-900 border-b-red-800"}`} onClick={() => navigate("/offers")}>Offers</li>
+                    <li className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${(pathMatchRoute('/sign-in') || pathMatchRoute('/profile')) && "text-blue-900 border-b-red-800"}`} onClick={() => navigate("/profile")}>{pageName}</li>
                 </ul>
             </div>
         </header>
--- realtor-clone/src/pages/Profile.jsx
+++ realtor-clone/src/pages/Profile.jsx
@@ -1,9 +1,13 @@
-import { getAuth } from 'firebase/auth';
+import { getAuth, updateProfile } from 'firebase/auth';
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
+import {toast} from 'react-toastify';
+import {doc, updateDoc} from 'firebase/firestore';
+import {db} from '../firebase'
 
 function Profile() {
 
+  const [changeDetail , setChangeDetail] = useState(false);
   const auth = getAuth();
   const navigate = useNavigate();
 
@@ -18,6 +22,30 @@ function Profile() {
   }
   const {name , email } = formData;
 
+  const onChange = (event) => {
+    setFormData((prevState) => ({
+      ...prevState,
+        [event.target.id] : event.target.value
+    }))
+  }
+
+  const onsubmit = async () => {
+    try {
+      // update displayname in firebase auth
+      await updateProfile(auth.currentUser , {
+        displayName : name
+      })
+      // update the name for firestore 
+      const docRef = doc(db , "users" , auth.currentUser.uid)
+      await updateDoc(docRef , {
+        name
+      })
+      toast.success("Updated successful")
+    } catch (error) {
+      toast.error("Couldn't update the profile")
+    }
+  }
+
   return (
     <div>
       <section className='max-w-6xl mx-auto flex justify-center items-center flex-col'>
@@ -26,13 +54,16 @@ function Profile() {
           <form>
 
               {/* Name input */}
-            <input type="text" id="name" value={name} disabled className='w-full mb-6 px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out'/>
+            <input type="text" id="name" onChange={onChange} value={name} disabled = {!changeDetail} className={`w-full mb-6 px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out ${changeDetail && "bg-blue-300 focus:bg-blue-100"}`}/>
             {/* Email input */}
             <input type="email" id="email" value={email} disabled className='w-full mb-6 px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out'/>
           
             <div className='flex justify-between text-sm sm:text-lg whitespace-nowrap'>
               <p>Do you want to change your name ?
-                <span className='text-red-700 hover:text-red-500 transition ease-in-out duration-200 cursor-pointer ml-2'>Edit</span>
+                <span onClick= {() => {
+                  changeDetail && onsubmit(); 
+                  setChangeDetail((prevState) => !prevState)
+                }} className='text-red-700 hover:text-red-500 transition ease-in-out duration-200 cursor-pointer ml-2'>{changeDetail ? "Edited" : "Edit"}</span>
               </p>
               <p onClick={onLogout} className='text-blue-600 hover:text-blue-800 transition ease-in-out duration-200 cursor-pointer'>Sign Out</p>
             </div>
