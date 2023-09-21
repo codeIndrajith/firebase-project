5cd84656485e171aac46cffa443ada9bef3d3129
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2023-09-21 22:35:30 +0530

    Add share copy functionality and more

--- realtor-clone/src/pages/Listing.jsx
+++ realtor-clone/src/pages/Listing.jsx
@@ -6,11 +6,13 @@ import Spinner from "../components/Spinner";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore , { Navigation, Pagination, Autoplay, EffectFade } from "swiper";
 import "swiper/css/bundle";
+import {FaShare} from 'react-icons/fa'
 
 function Listing() {
   const params = useParams();
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [shareLinkedCopied , setShareLinkedCopied] = useState(false);
   SwiperCore.use([Autoplay, EffectFade, Navigation, Pagination]);
   useEffect(() => {
     const fetchListing = async () => {
@@ -48,7 +50,25 @@ function Listing() {
           </SwiperSlide>
         ))}
       </Swiper>
+      <div className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center border-2 border-gray-400" onClick={() => {
+        navigator.clipboard.writeText(window.location.href);
+        setShareLinkedCopied(true)
+        setTimeout(() => {
+            setShareLinkedCopied(false)
+        }, 2000)
+      }}>
+        <FaShare className="text-lg text-slate-500"/>
+      </div>
+      {shareLinkedCopied && (
+        <p className="fixed font-bold top-[23%] right-[5%] z-10 bg-white border-2 border-gray-400 rounded-3xl w-[140px] h-[35px] flex justify-center items-center border-2 border-gray-400">Linked Copied</p>
+      )}
+
+      <div className="flex flex-col md:flex-row max-w-6xl lg:mx-auto p-4 rounded-lg shadow-lg lg:space-x-5">
+        <div className="bg-blue-600 w-full h-[200px] h-[400px]"></div>
+        <div className="bg-red-600 w-full h-[200px] h-[400px]"></div>
+      </div>
     </main>
+    
   );
 }
 
