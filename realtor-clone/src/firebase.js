1fde8881bc3ef2b7a68929e1af01923709bb639c
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2024-06-05 13:43:11 +0530

    Update

--- realtor-clone/src/firebase.js
+++ realtor-clone/src/firebase.js
@@ -1,12 +1,7 @@
 import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBQ2dLT6-YgD6_cAka_hiFFH8isXbzLCVM",
-  authDomain: "realtor-clone-react-b4f98.firebaseapp.com",
-  projectId: "realtor-clone-react-b4f98",
-  storageBucket: "realtor-clone-react-b4f98.appspot.com",
-  messagingSenderId: "498909730426",
-  appId: "1:498909730426:web:b98802a6ad6f2c76000dc3"
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
