145572f52b5f6b12be811b2f7d3edab5b004be99
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2024-06-05 13:41:46 +0530

    Update firebase config file

--- realtor-clone/src/firebase.js
+++ realtor-clone/src/firebase.js
@@ -1,5 +1,4 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBQ2dLT6-YgD6_cAka_hiFFH8isXbzLCVM",
@@ -8,6 +7,12 @@ const firebaseConfig = {
   storageBucket: "realtor-clone-react-b4f98.appspot.com",
   messagingSenderId: "498909730426",
   appId: "1:498909730426:web:b98802a6ad6f2c76000dc3"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
